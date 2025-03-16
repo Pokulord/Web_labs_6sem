a63c3132b41346c43ce5c7c554acc8bd90d8f179
Svyat <svat4856@gmail.com>
2025-03-16 15:41:45 +0500

    Added view render

--- Django_lab1/movies/movies/urls.py
+++ Django_lab1/movies/movies/urls.py
@@ -18,12 +18,13 @@ Including another URLconf
 """
 import statistics
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",include("moviesapp.urls"),)
 ]
 
 
