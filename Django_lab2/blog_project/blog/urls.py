a92b4cee28a188d53c085869114b14b971a14ef9
Svyat <svat4856@gmail.com>
2025-04-15 21:57:27 +0500

    Added BlogAPI (with any troubles)

--- Django_lab2/blog_project/blog/urls.py
+++ Django_lab2/blog_project/blog/urls.py
@@ -1,7 +1,12 @@
 from . import views
-from django.urls import path
+from django.urls import path, re_path
+from blog import views
+
+from django.conf import settings
 
 urlpatterns = [
     path('', views.PostList.as_view(), name= 'home'),
-    path('<slug:slug>/', views.PostDetail.as_view() , name='post_detail')
+    path('<slug:slug>/', views.PostDetail.as_view() , name='post_detail'),
+    re_path(r'^post$', views.PostAPI),
+    re_path(r'^post/(.+)$', views.PostAPI)
 ]
--- Django_lab2/blog_project/blog/views.py
+++ Django_lab2/blog_project/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from .serializers import PostSerializer
 
 # Create your views here.
 
@@ -13,3 +17,33 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+
+# Для запросов API
+
+@csrf_exempt
+def PostAPI(request, q_title = "Empty"):
+    if request.method == "GET":
+        posts = Post.objects.all()
+        posts_ser = PostSerializer(posts, many = True)
+        return JsonResponse(posts_ser.data, safe= False)
+    elif request.method == "POST":
+        post_data = JSONParser().parse(request)
+        post_ser = PostSerializer(data = post_data)
+        if post_ser.is_valid():
+            post_ser.save()
+            return JsonResponse("Added successfully", safe = False)
+        return JsonResponse("Failed to add " , safe= False)
+    elif request.method == "PUT":
+        post_data = JSONParser().parse(request)
+        post = Post.objects.get(title=post_data['title'])
+        post_ser = PostSerializer(post, data = post_data)
+        if post_ser.is_valid():
+            post_ser.save()
+            return JsonResponse("Updated Successfully", safe= True)
+        return JsonResponse("Failed to update", safe= False)
+    elif request.method == "DELETE":
+        post = Post.objects.get(title=q_title)
+        post.delete()
+        return JsonResponse("Deleted Successfully", safe= False)
